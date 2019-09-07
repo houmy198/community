7929599e4c2ce43cff1ef12ae5001011d405529b
houmy198 <984418003@qq.com>
2019-09-07 21:48:30 +0800

    init repo

---
+++ src/main/java/com/houmy/community/HelloController.java
@@ -0,0 +1,19 @@
+package com.houmy.community;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by Administrator on 2019/9/7.
+ *
+ */
+@Controller
+public class HelloController {
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
