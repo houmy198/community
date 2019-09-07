409f52997baa84855c7121a0a3a2ef86e86adcc2
houmy198 <984418003@qq.com>
2019-09-07 21:55:22 +0800

    添加注释

--- src/main/java/com/houmy/community/CommunityApplication.java
+++ src/main/java/com/houmy/community/CommunityApplication.java
@@ -1,6 +1,7 @@
 package com.houmy.community;
 		import org.springframework.boot.SpringApplication;
 		import org.springframework.boot.autoconfigure.SpringBootApplication;
+//添加注释
 @SpringBootApplication
 public class CommunityApplication {
 	public static void main(String[] args) {
